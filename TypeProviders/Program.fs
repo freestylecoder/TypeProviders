e3e3d89a8c4a56b62074e8cbc8d79ce3f0032853
Chris Gardner <cgardner@gmail.com>
2017-05-31 16:18:35 -0500

    Added PPTX Added Config Type Provider

--- TypeProviders/Program.fs
+++ TypeProviders/Program.fs
@@ -1,9 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open TypeProviders.Config
+
 [<EntryPoint>]
 let main argv = 
-
+    
+    printAppSettings ()
+    
     printfn ""
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
--- TypeProviders/config.fs
+++ TypeProviders/config.fs
@@ -3,3 +3,12 @@
 module Config =
     open FSharp.Configuration
 
+    type AppConfig = AppSettings<"app.config">
+
+    let printAppSettings () =
+        printfn "%s" AppConfig.ConfigFileName
+
+        printfn "StringKey:  %s" AppConfig.StringKey
+        printfn "IntegerKey: %i" AppConfig.IntegerKey
+        printfn "FloatKey:   %f" AppConfig.FloatKey
+        printfn "BooleanKey: %b" AppConfig.BooleanKey
